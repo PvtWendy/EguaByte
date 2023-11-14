f0d7e629959c33fe90451da294a929159820aba6
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-11-14 10:33:57 -0400

    Made a button to execute JS for now

--- src/components/EguaEditor.tsx
+++ src/components/EguaEditor.tsx
@@ -1,22 +1,34 @@
-import Editor from '@monaco-editor/react';
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
 
 const EguaEditor = () => {
+  const [code, setCode] = useState<string | undefined>("// Start coding here...");
+
+  const handleExecute = () => {
+    try {
+      if (code !== undefined) {
+        const result = eval(code);
+        console.log(result);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div>
+      <div className="editorTopBar">
+        <button onClick={handleExecute}>Executar</button>
+      </div>
       <Editor
-        height="100vh"
-        width="100vw"
-        theme='vs-dark'
-        defaultLanguage="javascript"
-        defaultValue="// Start coding here..."
+         height="95vh"
+         width="70vw"
+         theme='vs-dark'
+         defaultLanguage="javascript"
+         defaultValue={code}
+         onChange={(value, event) => setCode(value)}
       />
     </div>
   );
-}
+};
 
 export default EguaEditor;
-
-
-
-
-
