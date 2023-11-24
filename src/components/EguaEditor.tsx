99f64ca164512a215bf6c556652f6971525524fc
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-11-24 14:28:45 -0400

    Made it execute on the page

--- src/components/EguaEditor.tsx
+++ src/components/EguaEditor.tsx
@@ -2,41 +2,50 @@ import Editor from "@monaco-editor/react";
 import { useState } from "react";
 import { DeleguaWeb } from "./DeleguaWeb";
 
-
 const EguaEditor = () => {
-  const [code, setCode] = useState<string>("// Start coding here...");
+  const [code, setCode] = useState<string>("escreva('Olá mundo')");
+  const [consoleResult, setConsoleResult] = useState<string[]>([]);
 
-
-  const handleEditorChange = (value : string | undefined) => {
+  const handleEditorChange = (value: string | undefined) => {
     setCode(value || "");
   };
 
+  const executeConsole = (result: string) => {
+    setConsoleResult((prevResult: string[]) => [...prevResult, result]);
+  };
 
   const handleExecute = async function () {
-    const delegua = new DeleguaWeb("", console.log());
+    const delegua = new DeleguaWeb("", executeConsole);
 
-    const codigo = code.split("\n")
+    const codigo = code.split("\n");
 
     const retornoLexador = delegua.lexador.mapear(codigo, -1);
-    const retornoAvaliadorSintatico =
-        delegua.avaliadorSintatico.analisar(retornoLexador, 0);
+    const retornoAvaliadorSintatico = delegua.avaliadorSintatico.analisar(
+      retornoLexador,
+      0
+    );
 
     await delegua.executar({ retornoLexador, retornoAvaliadorSintatico });
-};
-  
+  };
+
   return (
     <div>
       <div className="editorTopBar">
         <button onClick={handleExecute}>Executar</button>
       </div>
       <Editor
-         height="95vh"
-         width="70vw"
-         theme='vs-dark'
-         defaultLanguage="javascript"
-         value={code}
-         onChange={handleEditorChange}
+        height="85vh"
+        width="70vw"
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        value={code}
+        onChange={handleEditorChange}
       />
+      <div>
+        {consoleResult.map((element, index) => (
+          <p key={index}>{element}</p>
+        ))}
+      </div>
     </div>
   );
 };
