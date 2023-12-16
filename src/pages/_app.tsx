bad667c299406a81fb72ac8efbdb73a4078c8ce4
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-12-15 22:35:14 -0400

    Used contextProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { QuestionsProvider } from "./api/questionsContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QuestionsProvider>
+      <Component {...pageProps} />
+    </QuestionsProvider>
+  );
 }
