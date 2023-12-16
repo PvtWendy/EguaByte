a2ed268080a2278c75d0ae9115d14579d7a885b0
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-12-15 22:36:21 -0400

    Implemented Questions component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
-import EguaEditor from "@/components/EguaEditor"
+import EguaEditor from "@/components/EguaEditor";
+import Questions from "@/components/Questions";
+
+//TODO: Make a global state to control which exercise is currently being used.
+//TODO: Make exercises be tested by "EguaEditor" based on which Question is currently used
+//TODO: Make a button to run the exercise
+//TODO: Make a button to reset the exercise
+//TODO: Make a button to change the exercise
+
 
 export default function Home() {
   return (
     <>
-    <EguaEditor></EguaEditor>
+      <main>
+        <Questions></Questions>
+        <EguaEditor></EguaEditor>
+      </main>
     </>
-  )
+  );
 }
