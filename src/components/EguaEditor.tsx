44ecce368aaa820d8ebc38a7c1aaab927c43ce51
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-12-15 23:55:49 -0400

    Implemented changes to defaultCode when question changes

--- src/components/EguaEditor.tsx
+++ src/components/EguaEditor.tsx
@@ -1,11 +1,30 @@
 import Editor from "@monaco-editor/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DeleguaWeb } from "./DeleguaWeb";
+import { useQuestions } from "@/pages/api/questionsContext";
 
 const EguaEditor = () => {
   const [code, setCode] = useState<string>("escreva('Olá mundo')");
   const [consoleResult, setConsoleResult] = useState<string[]>([]);
+  const { questions } = useQuestions();
+  
+  useEffect(() => {
+    
+    setCode(getDefaultCode());
+  }, [questions.value]);
 
+  function getDefaultCode(): string {
+    switch (questions.value) {
+      case "Question 1":
+        return "// Default code for Question 1";
+      case "Question 2":
+        return "// Default code for Question 2";
+      case "Question 3":
+        return "// Default code for Question 3";
+      default:
+        return "escreva('Olá mundo')";
+    }
+  }
   const handleEditorChange = (value: string | undefined) => {
     setCode(value || "");
   };
