bc6f7691cd84051912751bbcaf93b1f067956233
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-12-21 22:25:21 -0400

    removed todos, changed component  name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,12 @@
 import EguaEditor from "@/components/EguaEditor";
-import Questions from "@/components/Questions";
-
-//TODO: Make a global state to control which exercise is currently being used.
-//TODO: Make exercises be tested by "EguaEditor" based on which Question is currently used
-//TODO: Make a button to run the exercise
-//TODO: Make a button to reset the exercise
-//TODO: Make a button to change the exercise
+import ExerciseMenu from "@/components/ExerciseMenu";
 
 
 export default function Home() {
   return (
     <>
       <main>
-        <Questions></Questions>
+        <ExerciseMenu></ExerciseMenu>
         <EguaEditor></EguaEditor>
       </main>
     </>
