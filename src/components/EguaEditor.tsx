312e79a7fd6e0e3c3af75c6ca30de5fb403aecdb
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2023-12-21 22:25:39 -0400

    Refactored to fit new Context

--- src/components/EguaEditor.tsx
+++ src/components/EguaEditor.tsx
@@ -8,76 +8,22 @@ const EguaEditor = () => {
   const [consoleResult, setConsoleResult] = useState<string[]>([]);
   const { questions } = useQuestions();
 
-  //Effect to update code when questions.value is changed
+  //Effect to update code when the Question is changed
   useEffect(() => {
-    setCode(getDefaultCode());
-  }, [questions.value]);
+    setCode(questions.questionArray[questions.questionNumber].defaultCode);
+    setConsoleResult([]);
+  }, [questions.questionNumber]);
 
-  //The code it updates to
-  function getDefaultCode(): string {
-    switch (questions.value) {
-      case "Question 1":
-        setConsoleResult([]);
-        return `
-        funcao somaMinMax(array){
-          // Escreva sua função aqui
-
-        }
-        `;
-      case "Question 2":
-        setConsoleResult([]);
-        return `
-        funcao taxaPosNeg(array){
-          // Escreva sua função aqui
-
-        }
-        `;
-      case "Question 3":
-        setConsoleResult([]);
-        return `
-        funcao inteiroSolitario(array){
-          // Escreva sua função aqui
-
-        }
-        `;
-      default:
-        return "escreva('Olá mundo')";
-    }
-  }
+  useEffect(() => {
+    renderTable();
+  }, [consoleResult]);
 
   // Function to append additional code based on the question
   const appendAdditionalCode = (
     currentCode: string,
-    questionValue: string
+    additionalCode: string | null
   ): string => {
-    switch (questionValue) {
-      case "Question 1":
-        return (
-          currentCode +
-          "\n" +
-          "somaMinMax([100, 5, 50, 10, 25])\n" +
-          "somaMinMax([5, 5, 5, 5, 5])\n" +
-          "somaMinMax([1, 2, 3, 4, 5])"
-        );
-      case "Question 2":
-        return (
-          currentCode +
-          "\n" +
-          "taxaPosNeg([-4, 3, -9 ,0, 4, 1])\n" +
-          "taxaPosNeg([-1, -2, -3, 4, 5, 6])\n" +
-          "taxaPosNeg([0, 0, 0 ,0 ,0, 0])"
-        );
-      case "Question 3":
-        return (
-          currentCode +
-          "\n" +
-          "inteiroSolitario([1, 2, 3 , 4, 3, 2 ,1])\n" +
-          "inteiroSolitario([1,2 ,3 ,2 ,1])\n" +
-          "inteiroSolitario([5, 5, 6, 4, 4])"
-        );
-      default:
-        return currentCode;
-    }
+    return currentCode + (additionalCode ? `\n${additionalCode}` : "");
   };
 
   //Editor changes handler
@@ -97,7 +43,10 @@ const EguaEditor = () => {
 
     const currentCode = code;
 
-    const updatedCode = appendAdditionalCode(currentCode, questions.value);
+    const updatedCode = appendAdditionalCode(
+      currentCode,
+      questions.questionArray[questions.questionNumber].additionalCode
+    );
 
     const codigo = updatedCode.split("\n");
 
@@ -110,111 +59,55 @@ const EguaEditor = () => {
     await delegua.executar({ retornoLexador, retornoAvaliadorSintatico });
   };
 
-  //Function to render the table that displays test results
+  // Function to render the table that displays test results
   const renderTable = () => {
-    switch (questions.value) {
-      case "Question 1":
-        return (
-          <table className="resultTable">
-            <thead>
-              <tr>
-                <th>Entrada</th>
-                <th>Saída Esperada</th>
-                <th>Saída Encontrada</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>100 5 50 10 25</td>
-                <td>90 180</td>
-                <td>{consoleResult[0]}</td>
-              </tr>
-              <tr>
-                <td>5 5 5 5 5</td>
-                <td>25 25</td>
-                <td>{consoleResult[1]}</td>
-              </tr>
-              <tr>
-                <td>1 2 3 4 5</td>
-                <td>10 14</td>
-                <td>{consoleResult[2]}</td>
-              </tr>
-            </tbody>
-          </table>
-        );
-      case "Question 2":
-        return (
-          <table className="resultTable">
-            <thead>
-              <tr>
-                <th>Entrada</th>
-                <th>Saída Esperada</th>
-                <th>Saída Encontrada</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>-4 3 -9 0 4 1</td>
-                <td>
-                  0.500000 <br /> 0.333333 <br /> 0.166667
-                </td>
-                <td>{consoleResult[0]} <br /> {consoleResult[1]} <br /> {consoleResult[2]}</td>
-              </tr>
-              <tr>
-                <td>-1 -2 -3 4 5 6</td>
-                <td>0.500000 <br /> 0.500000 <br /> 0</td>
-                <td>{consoleResult[3]} <br /> {consoleResult[4]} <br /> {consoleResult[5]}</td>
-              </tr>
-              <tr>
-                <td>0 0 0 0 0 0 0</td>
-                <td>0 <br />0 <br /> 1</td>
-                <td>{consoleResult[6]} <br /> {consoleResult[7]} <br /> {consoleResult[8]}</td>
-              </tr>
-            </tbody>
-          </table>
-        );
+    const currentQuestion = questions.questionArray[questions.questionNumber];
 
-      case "Question 3":
-        return (
-          <table className="resultTable">
-            <thead>
-              <tr>
-                <th>Entrada</th>
-                <th>Saída Esperada</th>
-                <th>Saída Encontrada</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1 2 3 4 3 2 1</td>
-                <td>4</td>
-                <td>{consoleResult[0]}</td>
-              </tr>
-              <tr>
-                <td>1 2 3 2 1</td>
-                <td>3</td>
-                <td>{consoleResult[1]}</td>
-              </tr>
-              <tr>
-                <td>5 5 6 4 4</td>
-                <td>6</td>
-                <td>{consoleResult[2]}</td>
-              </tr>
-            </tbody>
-          </table>
-        );
-
-      default:
-        return (
-         
-            consoleResult.map((element, index) => (
-              <p key={index}>{element}</p>
-            ))
-       
-        );
+    //Renders the free console
+    if (!currentQuestion || questions.questionNumber === 0) {
+      return (
+        <div>
+          {consoleResult.map((item, index) => (
+            <p key={index}>{item}</p>
+          ))}
+        </div>
+      );
     }
+
+    const { input, expectedOutput } = currentQuestion;
+    const consoleResults = consoleResult.slice(0, expectedOutput?.length ?? 0);
+
+    // Combine expectedOutput and consoleResults into a single array
+    const combinedResults = (expectedOutput || []).map((item, index) => ({
+      expected: item,
+      result: consoleResults[index],
+    }));
+    
+    return (
+      <div>
+        <table className="resultTable">
+          <thead>
+            <tr>
+              {input !== null && <th>Entrada</th>}
+              {expectedOutput !== null && <th>Saída Esperada</th>}
+              <th>Saída Encontrada</th>
+            </tr>
+          </thead>
+          <tbody>
+            {combinedResults?.map(({ expected, result }, index) => (
+              <tr key={index} className={result === expected ? "green" : ""}>
+                {input !== null && <td>{input[index]}</td>}
+                <td>{expected}</td>
+                <td>{result}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
   };
 
+  //Renders component
   return (
     <div className="editorContainer">
       <div className="editorTopBar">
